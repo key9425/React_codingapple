faca647960589348965293991640cf0a279a7379
김은영 <kimeunyoung-0425@naver.com>
2025-01-27 00:09:34 +0900

    JSX 문법

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  let post = "강남 우동 맛집";
 
   return (
-    <div>
-      
+    // JSX 문법 1. class 넣을 땐 -> className
+    // JSX 문법 2. 변수 넣을 땐 (데이터바인딩) -> {변수명}
+    // JSX 문법 3. style 넣을 땐 -> style={{이름:'값'}}
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{ color: "red", fontSize: "16px" }}>개발 블로그</h4>
+      </div>
+      <h4 className={post}>{post}</h4>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
