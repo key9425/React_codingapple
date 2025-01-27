cf0af6576defaecd4caaaae860f06c9a8f223676
김은영 <kimeunyoung-0425@naver.com>
2025-01-27 17:42:18 +0900

    array자료 state 변경&정렬

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   // state는 변경되면 state와 관련된 html을 자동 재렌더링 - 변경될 것 같은 데이터
   // state 변경하는 법 -> state변경함수를 사용하여야 html 재렌더링 됨
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬 독학"]);
+  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬 독학"]);
   let [좋아요, 좋아요변경] = useState(0);
 
   return (
@@ -20,10 +20,23 @@ function App() {
         <h4 style={{ color: "red", fontSize: "16px" }}>개발 블로그</h4>
       </div>
 
+      {/* array, object state 변경 */}
+      <button
+        onClick={()=>{
+          let copy = [...글제목];
+          copy[0] = "여자코트 추천";
+          글제목변경(copy);
+        }}>글수정</button>
+      
+      {/* array 정렬 */}
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>정렬</button>
+
       <div className="list">
-        <h4>
-          {글제목[0]} <span onClick={()=>{좋아요변경(좋아요+1)}}>👍</span> {좋아요}
-        </h4>
+        <h4>{글제목[0]} <span onClick={()=>{좋아요변경(좋아요+1)}}>👍</span> {좋아요}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
