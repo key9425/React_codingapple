17a2c02d0a0511961fa26662d60778ef54594682
김은영 <kimeunyoung-0425@naver.com>
2025-01-30 11:16:57 +0900

    프로젝트 생성 & Bootstrap 사용

---
+++ shop/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
